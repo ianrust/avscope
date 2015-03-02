6c0ec40d97151ff4ded54b182118cc95bf994033
Ian Rust <iancrust@gmail.com>
2015-03-02 17:28:27 +0000

    fixed circle bugs, now displays the glitch mob log

--- test_gen.cpp
+++ test_gen.cpp
@@ -4,16 +4,16 @@
 
 void createCircle(float center_x,float center_y,float r,float start_angle,Points * points)
 {
-    float dtheta = M_PI/float(points->length);
+    float dtheta = 2.0*M_PI/float(points->length);
 
     points->locations = new Point[points->length];
 
     int index = 0;
-    for (float angle = start_angle; angle < M_PI+start_angle; angle += dtheta)
+    for (float angle = start_angle; angle < 2.0*M_PI+start_angle; angle += dtheta)
     {
         Point radial_point;
-        radial_point.x = r * cos(angle);
-        radial_point.x = r * sin(angle);
+        radial_point.x = r * cos(angle) + center_x;
+        radial_point.y = r * sin(angle) + center_y;
 
         points->locations[index] = radial_point;
 
@@ -30,7 +30,7 @@ int main (int argc, char ** argv)
 
     Points vert_line;
     vert_line.length = 2;
-    vert_line.max_val = 1.0;
+    vert_line.max_val = 1.3;
     vert_line.locations = new Point[2];
     vert_line.locations[0] = Point(0.0,-1.0);
     vert_line.locations[1] = Point(0.0,1.0*sqrt(3)-1.0);
@@ -40,7 +40,7 @@ int main (int argc, char ** argv)
 
     Points small_triangle;
     small_triangle.length = 3;
-    small_triangle.max_val = 1.0;
+    small_triangle.max_val = 1.3;
     small_triangle.locations = new Point[3];
     small_triangle.locations[0] = Point(0.0,-1.0);
     small_triangle.locations[1] = Point(-0.5,sqrt(3)/2.0-1.0);
@@ -51,7 +51,7 @@ int main (int argc, char ** argv)
 
     Points triangle;
     triangle.length = 4;
-    triangle.max_val = 1.0;
+    triangle.max_val = 1.3;
     triangle.locations = new Point[4];
     triangle.locations[0] = Point(0.0,-1.0);
     triangle.locations[1] = Point(-1.0,-1.0);
@@ -63,14 +63,14 @@ int main (int argc, char ** argv)
 
     Points left_circle;
     left_circle.length = 100;
-    left_circle.max_val = 1.0;
+    left_circle.max_val = 1.3;
     createCircle(-0.5,sqrt(3)/2.0-1.0,1.0,-2.0*M_PI/6.0,&left_circle);
     WaveForm wf_left_circle;
     assembleWaveform(&left_circle,&wf_left_circle,&info,true);
 
     Points right_circle;
     right_circle.length = 100;
-    right_circle.max_val = 1.0;
+    right_circle.max_val = 1.3;
     createCircle(0.5,sqrt(3)/2.0-1.0,1.0,-4.0*M_PI/6.0,&right_circle);
     WaveForm wf_right_circle;
     assembleWaveform(&right_circle,&wf_right_circle,&info,true);
