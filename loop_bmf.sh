090ae2b65278bb6c0009cb118f878ab02de83565
Ian Rust <iancrust@gmail.com>
2015-11-14 19:54:37 -0800

    added bmf script

---
+++ loop_bmf.sh
@@ -0,0 +1,10 @@
+while :
+do
+	svg2scope ~/avscope/resources/example_resources/bmf.svg 2 500 250 44100 2
+	sleep 1
+	svg2scope ~/avscope/resources/example_resources/butt.svg 2 500 250 44100 2
+	sleep 1
+	svg2scope ~/avscope/resources/clock/1.svg 2 500 250 44100 2
+	svg2scope ~/avscope/resources/clock/2.svg 2 500 250 44100 2
+	svg2scope ~/avscope/resources/clock/3.svg 2 500 250 44100 2
+done
